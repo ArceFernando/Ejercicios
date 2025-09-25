f94673988d6b2f3c6fa21803b8004b09ede47b88
ArceFernando <arce.fernando.ezequiel@gmail.com>
2025-09-24 23:42:19 -0300

    Permite Ver que campo tengo y modificarlo, las materias las tengo que cambiar a todas o dejarlas a todas

--- 009_ejercicio_struct_persona_dinamica_validaciones.c
+++ 009_ejercicio_struct_persona_dinamica_validaciones.c
@@ -152,7 +152,6 @@ Persona *crearPersona()
 
     return p;
 }
-
 void modificarPersona(Persona *p)
 {
     if (!p)
@@ -162,29 +161,170 @@ void modificarPersona(Persona *p)
     }
 
     printf("Modificando persona (ID %d):\n", p->id);
-    leerCadenaSoloLetras(p->nombre, MAX_NOMBRE, "Nuevo nombre: ");
-    leerCadenaSoloLetras(p->apellido, MAX_APELLIDO, "Nuevo apellido: ");
-    p->edad = leerEdad("Nueva edad (0 - 120): ");
-    leerCadenaSoloLetras(p->carrera, MAX_CARRERA, "Nueva carrera: ");
 
-    int n = leerEntero("Cuantas materias desea ingresar? (max 5): ");
-    if (n < 0)
-        n = 0;
-    else if (n > MAX_MATERIAS)
-        n = MAX_MATERIAS;
+    char buffer[100];
 
-    for (int i = 0; i < n; i++)
+    // Nombre
+    while (1)
     {
-        char mat[MAX_MATERIA_NOMBRE];
-        char msg[60];
-        snprintf(msg, sizeof(msg), "Nueva materia %d: ", i + 1);
-        leerCadenaSoloLetras(mat, MAX_MATERIA_NOMBRE, msg);
-        strncpy(p->materias[i], mat, MAX_MATERIA_NOMBRE - 1);
-        p->materias[i][MAX_MATERIA_NOMBRE - 1] = '\0';
+        printf("Nombre actual: %s\n", p->nombre);
+        printf("Nuevo nombre (enter para mantener): ");
+        leerCadena(buffer, sizeof(buffer));
+        if (strlen(buffer) == 0)
+        {
+            // Mantener actual
+            break;
+        }
+        else if (!esCadenaSoloLetras(buffer))
+        {
+            printf("Solo se permiten letras y espacios. Intente de nuevo.\n");
+        }
+        else
+        {
+            strncpy(p->nombre, buffer, MAX_NOMBRE - 1);
+            p->nombre[MAX_NOMBRE - 1] = '\0';
+            break;
+        }
     }
 
-    for (int i = n; i < MAX_MATERIAS; i++)
-        p->materias[i][0] = '\0';
+    // Apellido
+    while (1)
+    {
+        printf("Apellido actual: %s\n", p->apellido);
+        printf("Nuevo apellido (enter para mantener): ");
+        leerCadena(buffer, sizeof(buffer));
+        if (strlen(buffer) == 0)
+        {
+            break;
+        }
+        else if (!esCadenaSoloLetras(buffer))
+        {
+            printf("Solo se permiten letras y espacios. Intente de nuevo.\n");
+        }
+        else
+        {
+            strncpy(p->apellido, buffer, MAX_APELLIDO - 1);
+            p->apellido[MAX_APELLIDO - 1] = '\0';
+            break;
+        }
+    }
+
+    // Edad
+    while (1)
+    {
+        printf("Edad actual: %d\n", p->edad);
+        printf("Nueva edad (enter para mantener): ");
+        leerCadena(buffer, sizeof(buffer));
+        if (strlen(buffer) == 0)
+        {
+            break;
+        }
+        else if (esNumeroEntero(buffer))
+        {
+            int edad = atoi(buffer);
+            if (edad >= 0 && edad <= 120)
+            {
+                p->edad = edad;
+                break;
+            }
+            else
+                printf("Edad invalida. Debe estar entre 0 y 120.\n");
+        }
+        else
+        {
+            printf("Entrada invalida. Ingrese un numero entero.\n");
+        }
+    }
+
+    // Carrera
+    while (1)
+    {
+        printf("Carrera actual: %s\n", p->carrera);
+        printf("Nueva carrera (enter para mantener): ");
+        leerCadena(buffer, sizeof(buffer));
+        if (strlen(buffer) == 0)
+        {
+            break;
+        }
+        else if (!esCadenaSoloLetras(buffer))
+        {
+            printf("Solo se permiten letras y espacios. Intente de nuevo.\n");
+        }
+        else
+        {
+            strncpy(p->carrera, buffer, MAX_CARRERA - 1);
+            p->carrera[MAX_CARRERA - 1] = '\0';
+            break;
+        }
+    }
+
+    // Materias
+    printf("Materias actuales:\n");
+    int materiasActuales = 0;
+    for (int i = 0; i < MAX_MATERIAS; i++)
+    {
+        if (p->materias[i][0] != '\0')
+        {
+            printf("  %d. %s\n", i + 1, p->materias[i]);
+            materiasActuales++;
+        }
+    }
+    if (materiasActuales == 0)
+        printf("  (Ninguna)\n");
+
+    int n;
+    while (1)
+    {
+        printf("Cuantas materias desea ingresar? (0 a %d, enter para mantener actuales): ", MAX_MATERIAS);
+        leerCadena(buffer, sizeof(buffer));
+        if (strlen(buffer) == 0)
+        {
+            // Mantener actuales
+            n = materiasActuales;
+            break;
+        }
+        else if (esNumeroEntero(buffer))
+        {
+            n = atoi(buffer);
+            if (n >= 0 && n <= MAX_MATERIAS)
+                break;
+            else
+                printf("Numero invalido. Intente de nuevo.\n");
+        }
+        else
+        {
+            printf("Entrada invalida. Intente de nuevo.\n");
+        }
+    }
+
+    if (strlen(buffer) != 0) // Si ingresó un nuevo número de materias, modificar
+    {
+        for (int i = 0; i < n; i++)
+        {
+            while (1)
+            {
+                char msg[60];
+                snprintf(msg, sizeof(msg), "Materia %d: ", i + 1);
+                printf("%s", msg);
+                leerCadena(buffer, sizeof(buffer));
+                if (strlen(buffer) == 0)
+                {
+                    printf("La materia no puede estar vacia.\n");
+                    continue;
+                }
+                if (!esCadenaSoloLetras(buffer))
+                {
+                    printf("Solo se permiten letras y espacios. Intente de nuevo.\n");
+                    continue;
+                }
+                strncpy(p->materias[i], buffer, MAX_MATERIA_NOMBRE - 1);
+                p->materias[i][MAX_MATERIA_NOMBRE - 1] = '\0';
+                break;
+            }
+        }
+        for (int i = n; i < MAX_MATERIAS; i++)
+            p->materias[i][0] = '\0';
+    }
 
     printf("Persona modificada.\n");
 }
